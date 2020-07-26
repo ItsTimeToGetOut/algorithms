85d594ab21d41df32db3fcd52f8bfe0dfd368ef9
Shubhendu Singh <shubhendusingh15@gmail.com>
2020-07-26 22:25:35 +0530

    brute

--- Main.java
+++ Main.java
@@ -35,6 +35,26 @@ class Main {
       return false;
   }
 
+      public int[] rotateArray(int[] nums, int k) {
+        int n[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+            n[(i+k)%(nums.length)]=nums[i];
+        return n;
+    }
+
+      public void moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==0){
+                int j=i+1;
+                while(j<nums.length&&nums[j]==0)++j;
+                if(j<nums.length){
+                nums[i]=nums[j];
+                nums[j]=0;
+                }
+            }
+        }
+    }
+
   public static void main(String[] args) {
 
     Main a = new Main();
